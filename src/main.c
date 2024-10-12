d4be81594180a94e130513880198b4e41b6a8ff3
jose5556 <josefilmeneses@gmail.com>
2024-10-12 05:30:12 +0100

    error message print

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 01:39:38 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/10/12 02:54:46 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/10/12 05:06:56 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,7 @@ int	main(int argc, char	*argv[])
 	stack_b = (t_list *) malloc(sizeof(t_list));
 	if (argc < 3)
 		return (1);
-	check_error(argc, argv);
+	check_error(argc, argv, &stack_a, &stack_b);
+	ft_printf("success");
+	return (0);
 }
