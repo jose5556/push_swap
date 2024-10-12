fd906d23204cd5c2773648eb0033a559fcf33d5a
jose5556 <josefilmeneses@gmail.com>
2024-10-12 05:29:23 +0100

    prototype funcs added

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 01:40:06 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/10/12 01:41:25 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/10/12 05:08:00 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 # define PUSH_SWAP_H
 
 # include "../libft/libft.h"
+
+#define ERROR_MESSAGE "Error\n"
+
+void	error_check(int	argc, char **argv, t_list **stack_a, t_list **stack_b);
+void	exit_program(t_list **stack_a, t_list **stack_b);
 
 #endif
