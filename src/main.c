75dd9df9f61d745872f2153b74bbf368bc253c08
jose5556 <josefilmeneses@gmail.com>
2024-10-12 03:38:46 +0100

    main created

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 01:39:38 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/10/12 02:19:31 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/10/12 02:54:46 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 int	main(int argc, char	*argv[])
 {
-	
+	t_list	*stack_a;
+	t_list	*stack_b;
+
+	stack_a = (t_list *) malloc(sizeof(t_list));
+	stack_b = (t_list *) malloc(sizeof(t_list));
+	if (argc < 3)
+		return (1);
+	check_error(argc, argv);
 }
