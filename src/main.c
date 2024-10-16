d77b749594f2ce841d853691aba8b22b5d77dee0
jose5556 <josefilmeneses@gmail.com>
2024-10-16 03:06:46 +0100

    nothing

--- src/main.c
+++ src/main.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/12 01:39:38 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/10/12 05:06:56 by joseoliv         ###   ########.fr       */
+/*   Created: 2024/10/16 02:45:46 by joseoliv          #+#    #+#             */
+/*   Updated: 2024/10/16 02:48:57 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	main(int argc, char	*argv[])
 	stack_b = (t_list *) malloc(sizeof(t_list));
 	if (argc < 3)
 		return (1);
-	check_error(argc, argv, &stack_a, &stack_b);
+	error_check(argc, argv, &stack_a, &stack_b);
 	ft_printf("success");
 	return (0);
 }
