b1a39933132b83de7a7565ad33ba12e56921f123
jose5556 <josefilmeneses@gmail.com>
2024-10-16 04:53:18 +0100

    inicialize lists to NULL

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 02:45:46 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/10/16 04:16:08 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/10/16 04:51:06 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@ int	main(int argc, char	*argv[])
 	t_list	*stack_a;
 	t_list	*stack_b;
 
-	stack_a = (t_list *) malloc(sizeof(t_list));
-	stack_b = (t_list *) malloc(sizeof(t_list));
+	stack_a = NULL;
+	stack_b = NULL;
 	if (argc < 3)
 		return (1);
 	error_check(argc, argv, &stack_a, &stack_b);
+	ft_lstprint(stack_a);
 	ft_printf("success\n");
 	return (0);
 }
