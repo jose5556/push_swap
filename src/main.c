f419fdfa7f7049f6d08eda6ce89e59bff65457ff
jose5556 <josefilmeneses@gmail.com>
2024-10-16 04:29:54 +0100

    nothing

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 02:45:46 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/10/16 02:48:57 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/10/16 04:16:08 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ int	main(int argc, char	*argv[])
 	if (argc < 3)
 		return (1);
 	error_check(argc, argv, &stack_a, &stack_b);
-	ft_printf("success");
+	ft_printf("success\n");
 	return (0);
 }
