cdfff02fa8915cc00283a362418b3567158f6429
jose5556 <josefilmeneses@gmail.com>
2024-10-19 06:38:19 +0100

    free_args added

--- src/errors_handler/exit.c
+++ src/errors_handler/exit.c
@@ -6,14 +6,26 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 06:14:44 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/10/16 06:15:52 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/10/19 06:11:25 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/push_swap.h"
 
-void	exit_program(void)
+void	free_args(char **args)
 {
+	int	i;
+
+	i = 0;
+	while (args[i])
+		free(args[i++]);
+	free(args);
+}
+
+void	exit_program(int flag, char **args)
+{
+	if (flag == 1)
+		free_args(args);
 	ft_printf("%s", ERROR_MESSAGE);
 	exit(EXIT_FAILURE);
 }
