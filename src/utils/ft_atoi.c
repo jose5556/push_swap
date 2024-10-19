1ca569b63aaa0c0ceef2d7e57ccc9736c812e52b
jose5556 <josefilmeneses@gmail.com>
2024-10-19 06:38:36 +0100

    prototype modified

--- src/utils/ft_atoi.c
+++ src/utils/ft_atoi.c
@@ -6,35 +6,36 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 05:38:20 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/10/16 06:16:37 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/10/19 06:28:39 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/push_swap.h"
 
-int	ft_atoi(const char *nptr)
+void	ft_atoi(char **argv, int argc, long result, int check_value)
 {
 	long	i;
-	long	result;
-	long	check_value;
+	long	j;
 
-	result = 0;
-	check_value = 1;
+	j = 0;
 	i = 0;
-	if (nptr[i] == '-')
+	while (argv[j])
 	{
-		check_value *= -1;
-		i++;
+		if (argv[j][i] == '-')
+		{
+			check_value *= -1;
+			i++;
+		}
+		else if (argv[j][i] == '+')
+			i++;
+		while (argv[j][i] >= 48 && argv[j][i] <= 57)
+		{
+			result = result * 10 + (argv[j][i] - '0');
+			if (result * check_value > INT_MAX
+				|| result * check_value < INT_MIN)
+				exit_program(argc, argv);
+			i++;
+		}
+		j++;
 	}
-	else if (nptr[i] == '+')
-		i++;
-	while (nptr[i] >= 48 && nptr[i] <= 57)
-	{
-		result = result * 10 + (nptr[i] - '0');
-		if (result * check_value > INT_MAX
-			|| result * check_value < INT_MIN)
-			exit_program();
-		i++;
-	}
-	return (result * check_value);
 }
