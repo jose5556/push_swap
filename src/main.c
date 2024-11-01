32116e5d272ecc2c707efcbe03f79c130643b873
jose5556 <josefilmeneses@gmail.com>
2024-11-01 04:48:50 +0000

    resolv prob if 2 or 3 args passed

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 02:45:46 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/10/21 14:11:13 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/01 04:46:01 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,31 @@ int	main(int argc, char	*argv[])
 {
 	t_list	*stack_a;
 	//t_list	*stack_b;
+	int		size;
 
 	stack_a = NULL;
 	//stack_b = NULL;
 	if (argc < 2)
 		return (1);
 	error_check(argc - 1, argv + 1, &stack_a);
-	ft_printf("success\n");
+	if (!is_sorted(stack_a))
+	{
+		size = ft_lstsize(stack_a);
+		if (size == 2)
+			ra(&stack_a);
+		else if (size == 3)
+			sort_three(&stack_a);
+	}
+	ft_lstprint(stack_a);
 	ft_lstclear(&stack_a);
+	//ft_lstclear(&stack_b);
 	return (0);
 }
+
+//write (1, "l", 1);
+
+/* 		
+		else if (size == 3)
+			//TODO
+		else
+			//TODO */
