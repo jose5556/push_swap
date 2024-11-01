7a66eab6eded90363218f7387cdb35af915c98a6
jose5556 <josefilmeneses@gmail.com>
2024-11-01 01:10:35 +0000

    rotate funcs prototype

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 01:40:06 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/10/29 22:20:31 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/01 01:09:55 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,10 +35,14 @@ void	free_args(char **args);
 void	ft_atoi(char **argv, int argc, long result, int check_value);
 int		is_sorted(t_list *stack_a);
 
+void	ra(t_list **stack_a);
+void	rb(t_list **stack_b);
+void	rr(t_list **stack_a, t_list **stack_b);
 
 void	ft_lstadd_back(t_list **lst, int content);
 void	ft_lstclear(t_list **lst);
 t_list	*ft_newlst(int content);
 size_t	ft_lstsize(t_list *lst);
 void	ft_lstprint(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 #endif
