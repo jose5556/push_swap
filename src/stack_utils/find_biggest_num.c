b92f52c6a766a547b851938d0a497cdc521fa2d1
jose5556 <josefilmeneses@gmail.com>
2024-11-01 04:47:56 +0000

    func to find the biggest num on a stack

---
+++ src/stack_utils/find_biggest_num.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   find_biggest_num.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 04:33:18 by joseoliv          #+#    #+#             */
+/*   Updated: 2024/11/01 04:36:43 by joseoliv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/push_swap.h"
+
+t_list *find_biggest_num(t_list *stack)
+{
+	t_list *temp;
+
+	temp = stack;
+	while(stack)
+	{
+		if (stack->num > temp->num)
+			temp = stack;
+		stack = stack->next;
+	}
+	return (temp);
+}
