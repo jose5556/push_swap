1dde294e10f995c722a1f3073393a7f31cab24bd
jose5556 <josefilmeneses@gmail.com>
2024-11-05 04:08:29 +0000

    reorganize

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/10/12 01:54:02 by joseoliv          #+#    #+#              #
-#    Updated: 2024/11/04 07:09:06 by joseoliv         ###   ########.fr        #
+#    Updated: 2024/11/05 01:52:58 by joseoliv         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,18 +16,18 @@ CC = cc -g
 CFLAGS = -Wall -Wextra -Werror
 
 SRC =	src/main.c src/errors_handler/error_verifications.c src/errors_handler/exit.c \
-		src/instructions/push.c src/instructions/reverse_rotate.c src/instructions/rotate.c  \
+		src/instructions/push.c src/instructions/reverse_rotate.c src/instructions/rotate.c \
 		src/instructions/swap.c \
 		src/utils/find_biggest_num.c src/utils/find_smallest_num.c src/utils/ft_atoi.c \
 		src/utils/ft_lstadd_back.c src/utils/ft_lstclear.c src/utils/ft_lstlast.c \
-		src/utils/ft_lstprint.c src/utils/ft_lstsize.c src/utils/ft_newlst.c\
-		src/utils/index_aboveline.c src/utils/is_sorted.c
+		src/utils/ft_lstprint.c src/utils/ft_lstsize.c src/utils/ft_newlst.c \
+		src/utils/index_aboveline.c src/utils/is_sorted.c \
 		src/sort_stack/init/init_a.c src/sort_stack/init/init_b.c \
 		src/sort_stack/move_nodes/a_to_b.c src/sort_stack/move_nodes/b_to_a.c \
-		src/sort_stack/sort_large_stack src/sort_stack/sort_three.c src/sort_stack/super_mini_sort.c
+		src/sort_stack/sort_large_stack.c src/sort_stack/sort_three.c src/sort_stack/super_mini_sort.c
 
 LIBFT_DIR = ./libft
-LIBFT = $(LIBFT_DIR)/libft.a
+LIBFT = $(LIBFT_DIR)/libft.a 
 
 OBJ = $(SRC:.c=.o)
 
--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 01:40:06 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/11/04 06:50:28 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/05 03:52:05 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,31 +32,23 @@ typedef struct s_list
 void	error_check(int argc, char **argv, t_list **stack_a);
 void	exit_program(int flag, char **args);
 void	free_args(char **args);
-
-void	init_a(t_list **stack_a, t_list **stack_b);
-void	init_b(t_list **stack_b, t_list **stack_a);
-
+void	init_a(t_list *stack_a, t_list *stack_b);
+void	init_b(t_list *stack_b, t_list *stack_a);
 void	move_a_to_b(t_list **stack_a, t_list **stack_b);
 void	move_b_to_a(t_list **stack_b, t_list **stack_a);
-
 void	ra(t_list **stack_a);
 void	rb(t_list **stack_b);
 void	rr(t_list **stack_a, t_list **stack_b);
-
 void	rra(t_list **stack_a);
 void	rrb(t_list **stack_b);
 void 	rrr(t_list **stack_a, t_list **stack_b);
-
 void	pa(t_list **stack_b, t_list **stack_a);
 void	pb(t_list **stack_a, t_list **stack_b);
-
 void	sa(t_list **stack_a);
 void	sb(t_list **stack_b);
-
 void	sort_three(t_list **stack);
 void	sort_stack(t_list **stack_a, t_list **stack_b);
 void	super_mini_sort(t_list **stack, t_list *to_move, int flag);
-
 void	ft_lstadd_back(t_list **lst, int content);
 void	ft_lstclear(t_list **lst);
 t_list	*ft_newlst(int content);
@@ -65,7 +57,7 @@ void	ft_lstprint(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 t_list	*find_biggest_num(t_list *stack);
 t_list	*find_smallest_num(t_list *stack);
-void	set_index(t_list **stack);
+void	set_index(t_list *stack);
 int		is_sorted(t_list *stack_a);
 void	ft_atoi(char **argv, int argc, long result, int check_value);
 
