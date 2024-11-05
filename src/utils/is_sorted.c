49ac425bd1d99995a6b4c2a396369ee00912d718
jose5556 <josefilmeneses@gmail.com>
2024-11-05 04:06:28 +0000

    new line aded

--- src/utils/is_sorted.c
+++ src/utils/is_sorted.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 21:15:22 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/10/29 23:19:37 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/05 03:57:08 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,11 @@
 
 int	is_sorted(t_list *stack_a)
 {
-	t_list	*temp;
-
+	if (!stack_a)
+		return (0);
 	while (stack_a->next)
 	{
-		temp = stack_a->next;
-		if (stack_a->num > temp->num)
+		if (stack_a->num > stack_a->next->num)
 			return (0);
 		stack_a = stack_a->next;
 	}
