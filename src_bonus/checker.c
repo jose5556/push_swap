e2dd44b9e7cfc3ead2f18dd89dee5389ca35d665
jose5556 <josefilmeneses@gmail.com>
2024-11-07 11:06:00 +0000

    removed init simple lst func

--- src_bonus/checker.c
+++ src_bonus/checker.c
@@ -6,107 +6,87 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 05:03:25 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/11/06 08:23:40 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/07 11:04:45 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/checker.h"
 
-void	exit_checker(t_list **a, t_list **b, t_simple_list **lst, char *command)
+int	check_command(char *command)
 {
-	if (*a)
-		ft_lstclear(a);
-	if (*b)
-		ft_lstclear(b);
-	if (*lst)
-		ft_simple_lstclear(lst);
-	if (*command)
-		free(command);
-	ft_printf("%s", ERROR_MESSAGE);
-	exit(EXIT_FAILURE);
-}
-
-int	check_command(char *command, t_simple_list *lst)
-{
-	int	len;
-
-	len = ft_strlen(command);
-	while (lst)
-	{
-		if (!ft_strncmp(command, lst->name, len - 1))
-			return (1);
-		lst = lst->next;
-	}
+if (!(ft_strncmp(command, "sa", 2)))	
+		return (1);
+	else if (!(ft_strncmp(command, "sb", 2)))
+		return (1);
+	else if (!(ft_strncmp(command, "ss", 2)))
+		return (1);
+	else if (!(ft_strncmp(command, "pa", 2)))
+		return (1);
+	else if (!(ft_strncmp(command, "pb", 2)))
+		return (1);
+	else if (!(ft_strncmp(command, "ra", 2)))
+		return (1);
+	else if (!(ft_strncmp(command, "rb", 2)))
+		return (1);
+	else if (!(ft_strncmp(command, "rr", 2)))
+		return (1);
+	else if (!(ft_strncmp(command, "rra", 3)))
+		return (1);
+	else if (!(ft_strncmp(command, "rrb", 3)))
+		return (1);
+	else if (!(ft_strncmp(command, "rrr", 3)))
+		return (1);
 	return (0);
 }
 
 void	exec_command(char *command, t_list **stack_a, t_list **stack_b)
 {
-	int	len;
-
-	len = ft_strlen(command);
-	if (!(ft_strncmp(command, "sa", len)))
+	if (!(ft_strncmp(command, "sa", 2)))	
 		sa(stack_a, 0);
-	else if (!(ft_strncmp(command, "sb", len)))
+	else if (!(ft_strncmp(command, "sb", 2)))
 		sb(stack_b, 0);
-	else if (!(ft_strncmp(command, "ss", len)))
+	else if (!(ft_strncmp(command, "ss", 2)))
 		ss(stack_a, stack_b, 0);
-	else if (!(ft_strncmp(command, "pa", len)))
+	else if (!(ft_strncmp(command, "pa", 2)))
 		pa(stack_b, stack_a, 0);
-	else if (!(ft_strncmp(command, "pb", len)))
+	else if (!(ft_strncmp(command, "pb", 2)))
 		pb(stack_a, stack_b, 0);
-	else if (!(ft_strncmp(command, "ra", len)))
+	else if (!(ft_strncmp(command, "ra", 2)))
 		ra(stack_a, 0);
-	else if (!(ft_strncmp(command, "rb", len)))
+	else if (!(ft_strncmp(command, "rb", 2)))
 		rb(stack_b, 0);
-	else if (!(ft_strncmp(command, "rr", len)))
+	else if (!(ft_strncmp(command, "rr", 2)))
 		rr(stack_a, stack_b, 0);
-	else if (!(ft_strncmp(command, "rra", len)))
+	else if (!(ft_strncmp(command, "rra", 3)))
 		rra(stack_a, 0);
-	else if (!(ft_strncmp(command, "rrb", len)))
+	else if (!(ft_strncmp(command, "rrb", 3)))
 		rrb(stack_b, 0);
-	else if (!(ft_strncmp(command, "rrr", len)))
+	else if (!(ft_strncmp(command, "rrr", 3)))
 		rrr(stack_a, stack_b, 0);
-}
-
-void	init_commands_name(t_simple_list **lst)
-{
-	ft_simple_lstadd_back(lst, "sa");
-	ft_simple_lstadd_back(lst, "sb");
-	ft_simple_lstadd_back(lst, "ss");
-	ft_simple_lstadd_back(lst, "pa");
-	ft_simple_lstadd_back(lst, "pb");
-	ft_simple_lstadd_back(lst, "ra");
-	ft_simple_lstadd_back(lst, "rb");
-	ft_simple_lstadd_back(lst, "rr");
-	ft_simple_lstadd_back(lst, "rra");
-	ft_simple_lstadd_back(lst, "rrb");
-	ft_simple_lstadd_back(lst, "rrr");
+	free(command);
 }
 
 int	main(int argc, char *argv[])
 {
-	t_list			*stack_a;
-	t_list			*stack_b;
-	t_simple_list	*commands_name;
-	char			*command;
+	t_list	*stack_a;
+	t_list	*stack_b;
+	char	*command;
+	int		flag;
 
+	flag = 0;
 	stack_a = NULL;
 	stack_b = NULL;
-	commands_name = NULL;
 	if (argc < 2)
 		return (1);
 	error_check(argc - 1, argv + 1, &stack_a);
-	init_commands_name(&commands_name);
-	while ((command = get_next_line(0)) && command)
+	while ((command = get_next_line(0)) && command && !flag)
 	{
-		if (!(check_command(command, commands_name)))
-			exit_checker(&stack_a, &stack_b, &commands_name, command);
+		if (!(check_command(command)))
+			flag = 1;
 		exec_command(command, &stack_a, &stack_b);
 	}
 	if (is_sorted(stack_a) && !(ft_lstsize(stack_b)))
-	{
-		ft_printf(OK_MESSAGE);
-		ft_lstclear(&stack_a);
-	}
+		exit_checker_success(&stack_a);
+	else
+		exit_checker_failure(&stack_a, &stack_b, command);
 }
