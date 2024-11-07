371ea759ce45a1a1fd9fc05a2adbb63d88edcdd5
jose5556 <josefilmeneses@gmail.com>
2024-11-07 12:44:52 +0000

    removed func to check commands

--- src_bonus/checker.c
+++ src_bonus/checker.c
@@ -6,64 +6,53 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 05:03:25 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/11/07 11:04:45 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/07 12:36:10 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/checker.h"
 
-int	check_command(char *command)
+int	ft_strcmp(char *s1, char *s2)
 {
-if (!(ft_strncmp(command, "sa", 2)))	
-		return (1);
-	else if (!(ft_strncmp(command, "sb", 2)))
-		return (1);
-	else if (!(ft_strncmp(command, "ss", 2)))
-		return (1);
-	else if (!(ft_strncmp(command, "pa", 2)))
-		return (1);
-	else if (!(ft_strncmp(command, "pb", 2)))
-		return (1);
-	else if (!(ft_strncmp(command, "ra", 2)))
-		return (1);
-	else if (!(ft_strncmp(command, "rb", 2)))
-		return (1);
-	else if (!(ft_strncmp(command, "rr", 2)))
-		return (1);
-	else if (!(ft_strncmp(command, "rra", 3)))
-		return (1);
-	else if (!(ft_strncmp(command, "rrb", 3)))
-		return (1);
-	else if (!(ft_strncmp(command, "rrr", 3)))
-		return (1);
+	int	i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
 	return (0);
 }
 
-void	exec_command(char *command, t_list **stack_a, t_list **stack_b)
+int	exec_command(char *command, t_list **stack_a, t_list **stack_b)
 {
-	if (!(ft_strncmp(command, "sa", 2)))	
-		sa(stack_a, 0);
-	else if (!(ft_strncmp(command, "sb", 2)))
-		sb(stack_b, 0);
-	else if (!(ft_strncmp(command, "ss", 2)))
-		ss(stack_a, stack_b, 0);
-	else if (!(ft_strncmp(command, "pa", 2)))
-		pa(stack_b, stack_a, 0);
-	else if (!(ft_strncmp(command, "pb", 2)))
-		pb(stack_a, stack_b, 0);
-	else if (!(ft_strncmp(command, "ra", 2)))
-		ra(stack_a, 0);
-	else if (!(ft_strncmp(command, "rb", 2)))
-		rb(stack_b, 0);
-	else if (!(ft_strncmp(command, "rr", 2)))
-		rr(stack_a, stack_b, 0);
-	else if (!(ft_strncmp(command, "rra", 3)))
+	if (!(ft_strcmp(command, "rra\n")))
 		rra(stack_a, 0);
-	else if (!(ft_strncmp(command, "rrb", 3)))
+	else if (!(ft_strcmp(command, "rrb\n")))
 		rrb(stack_b, 0);
-	else if (!(ft_strncmp(command, "rrr", 3)))
+	else if (!(ft_strcmp(command, "rrr\n")))
 		rrr(stack_a, stack_b, 0);
-	free(command);
+	else if (!(ft_strcmp(command, "sa\n")))
+		sa(stack_a, 0);
+	else if (!(ft_strcmp(command, "sb\n")))
+		sb(stack_b, 0);
+	else if (!(ft_strcmp(command, "ss\n")))
+		ss(stack_a, stack_b, 0);
+	else if (!(ft_strcmp(command, "pa\n")))
+		pa(stack_b, stack_a, 0);
+	else if (!(ft_strcmp(command, "pb\n")))
+		pb(stack_a, stack_b, 0);
+	else if (!(ft_strcmp(command, "ra\n")))
+		ra(stack_a, 0);
+	else if (!(ft_strcmp(command, "rb\n")))
+		rb(stack_b, 0);
+	else if (!(ft_strcmp(command, "rr\n")))
+		rr(stack_a, stack_b, 0);
+	else
+		return (0);
+	return (1);
 }
 
 int	main(int argc, char *argv[])
@@ -79,14 +68,14 @@ int	main(int argc, char *argv[])
 	if (argc < 2)
 		return (1);
 	error_check(argc - 1, argv + 1, &stack_a);
-	while ((command = get_next_line(0)) && command && !flag)
+	while ((command = get_next_line(0)) && command)
 	{
-		if (!(check_command(command)))
+		if (!(exec_command(command, &stack_a, &stack_b)))
 			flag = 1;
-		exec_command(command, &stack_a, &stack_b);
+		free(command);
 	}
-	if (is_sorted(stack_a) && !(ft_lstsize(stack_b)))
+	if (is_sorted(stack_a) && !(ft_lstsize(stack_b)) && !flag)
 		exit_checker_success(&stack_a);
 	else
-		exit_checker_failure(&stack_a, &stack_b, command);
+		exit_checker_failure(&stack_a, &stack_b, command, flag);
 }
