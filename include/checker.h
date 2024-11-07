24c63625d1346e7b4bc0fe5e5a5c3300680e886c
jose5556 <josefilmeneses@gmail.com>
2024-11-07 12:44:35 +0000

    strcmp prototype added

--- include/checker.h
+++ include/checker.h
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 05:04:08 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/11/07 10:59:37 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/07 12:31:27 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@
 # define OK_MESSAGE "OK\n"
 # define KO_MESSAGE "KO\n"
 
-void			exit_checker_failure(t_list **a, t_list **b, char *command);
-void			exit_checker_success(t_list **a);
-int				check_command(char *command);
-void			exec_command(char *command, t_list **stack_a, t_list **stack_b);
+void	exit_checker_failure(t_list **a, t_list **b, char *command, int flag);
+void	exit_checker_success(t_list **a);
+int		exec_command(char *command, t_list **stack_a, t_list **stack_b);
+int		ft_strcmp(char *s1, char *s2);
 
 #endif
