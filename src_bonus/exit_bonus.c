abcc1e01de222d8bc755c9a66a572bd59abea48c
jose5556 <josefilmeneses@gmail.com>
2024-11-07 11:06:14 +0000

    added some funcs to exit the program

---
+++ src_bonus/exit_bonus.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit_bonus.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 09:06:14 by joseoliv          #+#    #+#             */
+/*   Updated: 2024/11/07 10:58:02 by joseoliv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/checker.h"
+
+void	exit_checker_failure(t_list **a, t_list **b, char *command)
+{
+	free(command);
+	ft_lstclear(a);
+	ft_lstclear(b);
+	ft_printf("%s", KO_MESSAGE);
+	exit(EXIT_SUCCESS);
+}
+
+void	exit_checker_success(t_list **a)
+{
+	ft_lstclear(a);
+	ft_printf("%s", OK_MESSAGE);
+	exit(EXIT_SUCCESS);
+}
